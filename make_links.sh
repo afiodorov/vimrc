afbc83732bbcd9b7b698d9e7cc7a1a8b95b91254
Tom Artiom Fiodorov <a.fiodorov@yahoo.co.uk>
2024-06-30 22:31:50 +0100

    adding python.lua

--- make_links.sh
+++ make_links.sh
@@ -2,29 +2,11 @@
 set -ex
 DIR="$( cd "$( dirname "$0" )" && pwd )"
 
-vimpath="${HOME}/.vim/after/ftplugin"
-mkdir -p "${vimpath}"
-for file in "${DIR}"/*.vim
-do
-	filename=$(basename "${file}")
-	rm -f "${vimpath}/${filename}"
-	ln -s $file "${vimpath}/"
-done
-
-vimsyntax="${HOME}/.vim/after/syntax"
-mkdir -p "${vimsyntax}"
-for file in "${DIR}"/after/syntax/*.vim
-do
-	filename=$(basename "${file}")
-	rm -f "${vimsyntax}/${filename}"
-	ln -s $file "${vimsyntax}/"
-done
-
 declare -A map_files
-map_files[".bashrc"]=".bash_aliases"
 map_files["init.lua"]=".config/nvim/init.lua"
 map_files["bat_config"]=".config/bat/config"
 map_files["coc-settings.json"]=".config/nvim/coc-settings.json"
+map_files["python.lua"]=".config/nvim/after/ftplugin/python.lua"
 declare -A ignore_files=([".viminfo"]=1 [".vrapperrc"]=1 ["README.md"]=1 ["make_links.sh"]=1 ["tags"]=1)
 
 for file in "${DIR}"/*
